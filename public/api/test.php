bfda9d5eb2949cfa4c79df77b27b77dfe4cdf46a
Kordian Bober <BOBKOR@ad.bertrand.pl>
2023-05-13 16:45:09 +0200

    login

--- public/api/test.php
+++ public/api/test.php
@@ -1,5 +1,9 @@
 <?php
 
+if($_SERVER['REQUEST_METHOD'] != 'POST') return;
+
+
+
 echo $_SERVER["HTTP_REFERER"];
 
 $arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
--- public/api/zaloguj.php
+++ public/api/zaloguj.php
@@ -12,24 +12,29 @@ $login = $dane->login;
 $password = md5($dane->password);
 
 //REPLACE
-$query_run = $dbh->prepare("SELECT id,`password` FROM users where login = ?");
-$query_run->execute();
+$query_run = $dbh->prepare("SELECT id,`password` FROM users where login = ? and password = ?");
+$query_run->execute([$login,$password]);
 
 $rows = $query_run->fetchAll(PDO::FETCH_ASSOC);
 
-if (password_verify($rows[0]->password, $dane->password)){
+//print_r($rows[0]);
+//echo $rows[0]['id'];
+
+//echo json_encode($rows);
+
+// if (password_verify($rows[0]->password, $dane->password)){
+//     $_SESSION['zalogowany'] = true;
+//     $_SESSION['id'] = $rows[0]->id;
+// }
+
+
+if(count($rows)>0){
     $_SESSION['zalogowany'] = true;
-    $_SESSION['id'] = $rows[0]->id;
-}
+    $_SESSION['id'] = $rows[0]['id'];
 
-
-//if(count($rows)>0){
-  //  $_SESSION['zalogowany'] = true;
-   // $_SESSION['id'] = $rows[0]->id;
-
-    //echo 'ZALOGOWANY';
-//}else{
-   // return 0;
+    echo 'ZALOGOWANY';
+}else{
+    return 0;
 }
 
 
